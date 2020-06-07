80063a1d9b7c6ef9f4cd206753cc7e42bc23a501
Aomi Jokoji <aomijokoji@gmail.com>
2020-06-07 14:46:03 -0700

    Remove reference to "Products"

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -12,5 +12,5 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('projects', ProjectPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
