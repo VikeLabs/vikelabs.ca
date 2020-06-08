d69a5dace23d9a69bcc0c8d0826089b4972e01e8
Aomi Jokoji <aomijokoji@gmail.com>
2020-06-07 21:40:24 -0700

    Fix alt tag on VikeLabs logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = class extends React.Component {
         <div className="container" >
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '230px' }} />
+              <img src={logo} alt="VikeLabs" style={{ width: '230px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
