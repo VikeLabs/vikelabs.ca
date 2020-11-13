21dd4622f6a1a1272acffc264062d1aeb0d67ceb
Reed McIlwain <reedmcilwain@gmail.com>
2020-11-13 13:44:56 -0800

    TestBranch - Ran npm format

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
-import { LinkedinOption, Instagram, FacebookOption } from "grommet-icons";
+import { LinkedinOption, Instagram, FacebookOption } from 'grommet-icons'
 
 const iconStyle = {
-  fill: "black",
-  width: "1em",
-  height: "1em"
+  fill: 'black',
+  width: '1em',
+  height: '1em',
 }
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer
-        // className="footer has-background-black has-text-white-ter" 
-        style={{ backgroundColor: "#FFF192" }}
+        // className="footer has-background-black has-text-white-ter"
+        style={{ backgroundColor: '#FFF192' }}
       >
         <div className="content has-text-centered">
           <img
@@ -26,11 +26,9 @@ const Footer = class extends React.Component {
         </div>
         <div
           // className="content has-text-centered has-background-black has-text-white-ter"
-          style={{ backgroundColor: "#222B49" }}
+          style={{ backgroundColor: '#222B49' }}
         >
-          <div
-            className="container has-text-white-ter"
-          >
+          <div className="container has-text-white-ter">
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
@@ -62,28 +60,29 @@ const Footer = class extends React.Component {
               <div className="column is-4">
                 <div className="social">
                   <a title="facebook" href="https://www.facebook.com/Vikelabs">
-                    <FacebookOption
-                      style={iconStyle}
-                      alt="Facebook"
-                    />
+                    <FacebookOption style={iconStyle} alt="Facebook" />
                   </a>
                   <a title="instagram" href="https://instagram.com/vikelabs">
-                    <Instagram
-                      style={iconStyle}
-                      alt="Instagram"
-                      />
+                    <Instagram style={iconStyle} alt="Instagram" />
                   </a>
-                  <a title="linkedin" href="https://www.linkedin.com/company/vikelabs/">
-                    <LinkedinOption
-                      style={iconStyle}
-                      alt="Linkedin"
-                    />
+                  <a
+                    title="linkedin"
+                    href="https://www.linkedin.com/company/vikelabs/"
+                  >
+                    <LinkedinOption style={iconStyle} alt="Linkedin" />
                   </a>
                 </div>
 
                 <div>
                   <section>
-                    <p>VikeLabs is based at the University of Victora. We acknowledge with respect the Lekwungen peoples on whose traditional territory the University of Victoria stands, and the Songhees, Esquimalt and W̱SÁNEĆ peoples whose historical relationships with the land continue to this day.</p>
+                    <p>
+                      VikeLabs is based at the University of Victora. We
+                      acknowledge with respect the Lekwungen peoples on whose
+                      traditional territory the University of Victoria stands,
+                      and the Songhees, Esquimalt and W̱SÁNEĆ peoples whose
+                      historical relationships with the land continue to this
+                      day.
+                    </p>
                   </section>
                 </div>
               </div>
