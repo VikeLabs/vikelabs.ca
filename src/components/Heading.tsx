f01ed4e7e8fd62930b4323b3a3de47b98fcbbd4b
Aomi Jokoji <ajokoji@change.org>
2021-01-02 12:19:27 -0800

    fix: remove default margin/padding on header

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,9 +5,12 @@ import { typography, TypographyProps } from "styled-system";
 // TODO: simplier heading component (using single Heading component rather than multiple via a level / variants)
 export const Heading = styled.h1<TypographyProps>`
   ${typography};
+
   font-family: ${(props) => props.theme.fontFamily};
   font-size: 3.5em;
   font-weight: 500;
+  margin: 0;
+  padding: 0;
 `;
 
 export const SubHeading = styled.h2<TypographyProps>`
