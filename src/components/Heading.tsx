204f08aace8fd9c00e2f93e909ed7e5eccdedceb
Aomi Jokoji <ajokoji@change.org>
2021-01-01 20:13:10 -0800

    feat: add Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { typography, TypographyProps } from "styled-system";
+
+// TODO: simplier heading component (using single Heading component rather than multiple via a level / variants)
+export const Heading = styled.h1<TypographyProps>`
+  ${typography};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: 3.5em;
+  font-weight: 500;
+`;
+
+export const SubHeading = styled.h2<TypographyProps>`
+  ${typography};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-size: 2.5em;
+  font-weight: 500;
+  padding: 0;
+  margin: 0;
+`;
