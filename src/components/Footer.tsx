ebb37dca4e226b8881d26fb63156989895a4a677
Aomi Jokoji <ajokoji@change.org>
2021-01-02 12:18:53 -0800

    feat: make a bit responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,6 +11,8 @@ import {
   LayoutProps,
   space,
   SpaceProps,
+  typography,
+  TypographyProps,
 } from "styled-system";
 
 const FooterContainer = styled.footer<SpaceProps | LayoutProps | FlexboxProps>`
@@ -18,27 +20,44 @@ const FooterContainer = styled.footer<SpaceProps | LayoutProps | FlexboxProps>`
   ${flexbox}
   ${layout}
   display: flex;
+  padding: 0;
+  margin: 35px 0 35px 0;
 `;
 
-const StyledTitle = styled.span`
+const StyledTitle = styled.span<TypographyProps>`
   font-family: ${(props) => props.theme.fontFamily};
   font-size: 2em;
   font-weight: 300;
+  ${typography}
+`;
+
+const CopyrightText = styled.span`
+  font-family: ${(props) => props.theme.fontFamily};
+  font-weight: 300;
+`;
+
+const LogoWrapper = styled(Box)`
+  padding-top: 20px;
+  /* hides the logo when smaller than lg */
+  @media (max-width: ${(props) => props.theme.breakpoints.lg}) {
+    display: none;
+  }
 `;
 
 export const Footer = () => {
   return (
     <FooterContainer my="6" flexDirection={{ sm: "column", md: "row" }}>
       {/* TODO: logo / title */}
-      <Box>
+      <LogoWrapper>
         <VikeLabs />
-      </Box>
+      </LogoWrapper>
       {/* TODO: thin styling */}
       {/* TODO: make uvic into a hyperlink */}
-      <Box px="4" borderRight="2px gray solid">
+      <Box px="4" borderRight={{ md: "2px gray solid" }}>
         <Box maxWidth="620px">
-          <StyledTitle>
-            VikeLabs is based at the University of Victoria.
+          <StyledTitle fontSize={{ sm: "1.5em", md: "1.5em", lg: "2em" }}>
+            VikeLabs is based at the{" "}
+            <a href="https://uvic.ca">University of Victoria.</a>
           </StyledTitle>
           <Text>
             We acknowledge with respect the Lekwungen peoples on whose
@@ -51,7 +70,7 @@ export const Footer = () => {
       <Box p="4">
         {/* TODO: sitemap */}
         {/* TODO: copyright */}
-        <span>© {new Date().getFullYear()} VikeLabs</span>
+        <CopyrightText>© {new Date().getFullYear()} VikeLabs</CopyrightText>
       </Box>
     </FooterContainer>
   );
