b2bc58e55d98840af685d3bdfad5c990c7ae586f
Aomi Jokoji <ajokoji@change.org>
2021-01-02 12:17:45 -0800

    feat: add another placeholder project entry

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,6 +4,7 @@ import { Heading, SubHeading } from "../components/Heading";
 import { Text } from "../components/Text";
 import { Layout } from "../components/Layout";
 import { Box } from "../components/Box";
+import { Contact } from "../components/Contact";
 
 const ProjectsPage = () => {
   return (
@@ -24,6 +25,20 @@ const ProjectsPage = () => {
           exercitation ut exercitation irure Lorem deserunt laboris ad.
         </Text>
       </Box>
+      <Box>
+        <SubHeading>UVic Course Scraper</SubHeading>
+        <Text>
+          Ex excepteur occaecat minim commodo do laboris. Pariatur qui elit in
+          consectetur mollit quis. Mollit deserunt aliquip velit sunt cillum
+          tempor enim aliquip commodo. Ea enim et adipisicing officia aute sint
+          mollit eiusmod eiusmod. Consectetur cupidatat do ipsum ullamco
+          ullamco. Esse anim commodo qui Lorem laboris consequat. Consequat ea
+          Lorem voluptate velit magna esse tempor cillum eu est culpa elit.
+        </Text>
+      </Box>
+      <section>
+        <Contact />
+      </section>
     </Layout>
   );
 };
