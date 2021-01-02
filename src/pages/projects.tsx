1f7a4f94a340c9b67336d4a081cc19964c31372e
Aomi Jokoji <ajokoji@change.org>
2021-01-01 20:49:07 -0800

    feat: add placeholder content

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import { Header } from "../components/Header";
+import { Heading, SubHeading } from "../components/Heading";
+import { Text } from "../components/Text";
 import { Layout } from "../components/Layout";
+import { Box } from "../components/Box";
 
 const ProjectsPage = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Heading>Projects</Heading>
+      <Text>
+        Proident mollit voluptate aliqua mollit minim officia ullamco non
+        cupidatat dolor culpa sint aute. Sit reprehenderit eu eu ad enim aliquip
+        officia pariatur proident. Adipisicing ipsum ullamco ea aliqua id sunt
+        esse et consequat exercitation minim et.
+      </Text>
+      <Box>
+        <SubHeading>Passr</SubHeading>
+        <Text>
+          Id id irure eu commodo irure reprehenderit dolore deserunt adipisicing
+          sunt. Aliqua consequat officia consectetur quis aliqua anim cupidatat
+          pariatur nostrud ipsum tempor mollit. Lorem occaecat nostrud quis
+          exercitation ut exercitation irure Lorem deserunt laboris ad.
+        </Text>
+      </Box>
+    </Layout>
+  );
 };
 export default ProjectsPage;
