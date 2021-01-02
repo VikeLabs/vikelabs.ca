cfb4471ff4d8cdc982856f4a9afd8e96938c9cf2
Aomi Jokoji <ajokoji@change.org>
2021-01-02 14:36:19 -0800

    feat: setup netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,13 +91,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    // TODO: properly setup
-    // {
-    //   resolve: 'gatsby-plugin-netlify-cms',
-    //   options: {
-    //     modulePath: `${__dirname}/src/cms/cms.js`,
-    //   },
-    // },
+    `gatsby-plugin-netlify-cms`,
     // 'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
