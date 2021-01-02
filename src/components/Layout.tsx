9c1474a47260f1bb8674963842cbe01ecdce7dba
Aomi Jokoji <ajokoji@change.org>
2021-01-01 20:13:23 -0800

    feat: add layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,37 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
 
-import React from "react"
-import styled from 'styled-components'
-
-export const Layout = styled.div`
-    padding: 8px;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
 `;
+
+const Main = styled.main`
+  margin: 0;
+  padding: 0;
+  max-width: 1440px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const InnerMain = styled.div``;
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Layout = ({ children }: Props) => (
+  <ThemeProvider theme={theme}>
+    <Container>
+      <Main>
+        <Header />
+        <InnerMain>{children}</InnerMain>
+        <Footer />
+      </Main>
+    </Container>
+  </ThemeProvider>
+);
