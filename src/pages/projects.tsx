0b2d2f82ea47bc112e8430c11f1355ff1c9fdab4
Aomi Jokoji <ajokoji@change.org>
2021-01-01 19:17:47 -0800

    feat: init blank projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Layout } from "../components/Layout";
+
+const ProjectsPage = () => {
+  return <Layout></Layout>;
+};
+export default ProjectsPage;
