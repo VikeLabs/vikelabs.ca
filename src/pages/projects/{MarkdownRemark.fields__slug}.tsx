3a2f57158e322b870093af4f23ff119e7b23b3e2
Aomi Jokoji <ajokoji@change.org>
2021-01-03 13:09:54 -0800

    docs: ProjectTemplate docs

--- src/pages/projects/{MarkdownRemark.fields__slug}.tsx
+++ src/pages/projects/{MarkdownRemark.fields__slug}.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { graphql, PageProps } from "gatsby";
 import { Layout } from "../../components/Layout";
 import { ProjectBySlug } from "./__generated__/ProjectBySlug";
+import { Metadata } from "../../components/Metadata";
+
+// This template is used to create the route /projects/{slug}
+// The slug (ie. vikelabs.ca/projects/passr) is created in gatsby-node.js
 
 // Use Gatsby PageProps with the generated GraphQL type defs (from query below)
 type ProjectTemplateProps = PageProps<ProjectBySlug>;
@@ -9,6 +13,8 @@ type ProjectTemplateProps = PageProps<ProjectBySlug>;
 const ProjectTemplate = ({ data }: ProjectTemplateProps) => {
   return (
     <Layout>
+      {/* TODO: better metadata */}
+      <Metadata title={data.markdownRemark?.frontmatter?.title || ""} />
       <div>
         <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
