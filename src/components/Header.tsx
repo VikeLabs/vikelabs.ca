c0775a51985bad5f06638aac6426070cf4ef488b
Aomi Jokoji <ajokoji@change.org>
2021-01-24 14:46:36 -0800

    feat: make Header menu minimally responsive
    this should make the website minimally usable on mobile devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,30 @@
 import { Link } from "gatsby";
 import React from "react";
 import styled from "styled-components";
-import { space, SpaceProps } from "styled-system";
+import { flexbox, FlexboxProps, space, SpaceProps } from "styled-system";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { Box } from "./Box";
+import { Box, Flex } from "./Box";
 import { VikeLabs, VikeLabsLogo } from "./Logo";
 
-const HeaderContainer = styled.header<SpaceProps>`
+const HeaderContainer = styled.header<SpaceProps | FlexboxProps>`
   ${space}
+  ${flexbox}
   display: flex;
   width: 100%;
   justify-content: space-between;
   align-content: center;
+
+  border: 1px solid black;
 `;
 
-const Nav = styled.nav`
+const Nav = styled.nav<FlexboxProps>`
+  ${flexbox}
   display: flex;
-  justify-content: space-between;
 `;
 
 const NavLink = styled(Link)`
   text-decoration: none;
-  padding-left: 20px;
+  /* padding-left: 20px; */
   padding-right: 20px;
   font-family: ${(props) => props.theme.fontFamily};
   font-size: 1.5em;
@@ -38,14 +41,19 @@ export const Header = () => {
   // const pathname = typeof window !== undefined ? window.location.pathname : "";
 
   return (
-    <HeaderContainer px="40px" py="20px">
-      <Box>
+    <HeaderContainer
+      mx={{ sm: "20px", md: "30px", lg: "40px" }}
+      py="20px"
+      flexDirection={{ sm: "column", md: "row" }}
+      justifyContent="space-between"
+    >
+      <Flex justifyContent="flex-start">
         {/* <VikeLabsLogo color="green" /> */}
         <VikeLabs />
-      </Box>
-      <Box>
+      </Flex>
+      <Box border="solid 1px black" mt={{ sm: "20px", md: "0px", lg: "0px" }}>
         {/* TODO: menu */}
-        <Nav>
+        <Nav justifyContent="flex-start">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/projects">Projects</NavLink>
           {/* <GiHamburgerMenu size="1.5em" /> */}
