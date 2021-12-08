c6a15b811bedd60c501d09d1a22190067de68bb4
Aomi <aomijokoji@gmail.com>
2021-12-07 18:15:17 -0800

    fix: discord URL

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import Layout from '../../components/Layout'
-import { Slack } from 'grommet-icons';
+import React from "react";
+import { navigate } from "gatsby-link";
+import Layout from "../../components/Layout";
+import { Slack } from "grommet-icons";
 
 function encode(data) {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 export default class Index extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { isValidated: false }
+    super(props);
+    this.state = { isValidated: false };
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
+        "form-name": form.getAttribute("name"),
         ...this.state,
       }),
     })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
 
   render() {
     return (
@@ -43,13 +43,19 @@ export default class Index extends React.Component {
               <h1>Contact</h1>
               <h2>Discord</h2>
               <p>
-                We've moved to Discord! <a href="https://discord.gg/AWcEfYKjff">Join our Discord here!</a>
+                We've moved to Discord!{" "}
+                <a href="https://discord.gg/4hGsua4Nqs">
+                  Join our Discord here!
+                </a>
               </p>
-              <h2>Please contact us through our social media channels found in the footer below.</h2>
+              <h2>
+                Please contact us through our social media channels found in the
+                footer below.
+              </h2>
             </div>
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
