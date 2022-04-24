7cfe81e643665f4840224ece864178ae0a03d98f
WeiJun Syu <weijunsyu@gmail.com>
2022-04-24 13:12:57 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,31 @@
-import React, {useState, useEffect, createRef} from "react"
+import React, {useState, useEffect} from "react"
 import Layout from "../components/Layout/Layout"
 import styled from '@emotion/styled';
 import {COLORS} from '../styles/globalstyles/theme';
 import {globalStyle} from '../styles/globalstyles/globalStyles';
+import { v, maxq } from '../styles/globalstyles/variables';
 
 const Page = styled.body`
   background-color: var(--color-background, ${COLORS.background.light});
   color: var(--color-text, ${COLORS.text.light});
   overflow: hidden;
+`
+const WindowHeader = styled.div`
+  background: var(--color-comment, ${COLORS.comment.light});
+  padding: 3px;
+  display: flex;
+  justify-content: end;
+  height: 1.2em;
+`
+const TerminalContainer = styled.div`
+  background: var(--color-background, ${COLORS.background.light});
+  padding: calc(${v.mdSpacing} / 1.5) ${v.mdSpacing} ${v.mdSpacing};
+  border-width: 0 3px 3px 3px;
+  border-style: solid;
+  border-color: var(--color-comment, ${COLORS.comment.light});
+  font-family: 'Fira Code', monospace;
+  font-weight: 500;
+  margin-bottom: 1em;
 `
 const Opening = styled.section`
   background-color: var(--color-comment, ${COLORS.comment.light});
@@ -47,10 +65,10 @@ const Ending = styled.section`
 
 const About = () => {
 
-  //const [windowSize, setWindowSize] = useState(1000);
+  const [windowSize, setWindowSize] = useState(null);
 
-  function getNumLines(content) {    
-    if (window !== undefined) {
+  function getNumLines(content) {
+    if (typeof window !== "undefined") {
       let style = window.getComputedStyle(content, null);
       let fontWidth = 0.64 * parseFloat(style.getPropertyValue('font-size'));
       let width = parseFloat(style.getPropertyValue('width'));
@@ -95,21 +113,24 @@ const About = () => {
   return (
     <Layout title="About">
       <Page>
-        <Opening>
-          <h1>We are VikeLabs.</h1>
-        </Opening>
-        <ContentBox>
-          <LineNumbers id="lineNumbers"></LineNumbers>
-          <Content id="content">
-            VikeLabs is a collective of students who learn to build, deploy, and test software quickly. We view UVic as a kind of laboratory for testing solutions to problems that exist within the UVic community. We limit ourselves to the UVic community because it's much easier to deploy and test solutions to users where we are in close proximity to them and their problems.
-            <br></br>
-            <br></br>
-            We accept members from every faculty who have an interest in product design/research, software development, business, marketing, or product management.
-          </Content>
-          </ContentBox>
-        <Ending>
-          <h2>Join us.</h2>
-        </Ending>
+        <WindowHeader/>
+        <TerminalContainer>
+          <Opening>
+            <h1>We are VikeLabs.</h1>
+          </Opening>
+          <ContentBox>
+            <LineNumbers id="lineNumbers"></LineNumbers>
+            <Content id="content">
+              VikeLabs is a collective of students who learn to build, deploy, and test software quickly. We view UVic as a kind of laboratory for testing solutions to problems that exist within the UVic community. We limit ourselves to the UVic community because it's much easier to deploy and test solutions to users where we are in close proximity to them and their problems.
+              <br></br>
+              <br></br>
+              We accept members from every faculty who have an interest in product design/research, software development, business, marketing, or product management.
+            </Content>
+            </ContentBox>
+          <Ending>
+            <h2>Join us.</h2>
+          </Ending>
+        </TerminalContainer>
       </Page>
     </Layout>
   )
