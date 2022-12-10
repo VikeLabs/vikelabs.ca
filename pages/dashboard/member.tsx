89108bc335664a49bac896c6c39d492c272f885a
robchendev <robchendev@gmail.com>
2022-12-10 03:50:35 -0800

    A better layout for the user info editor

--- pages/dashboard/member.tsx
+++ pages/dashboard/member.tsx
@@ -1,10 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Control, Controller, useForm } from "react-hook-form";
 import DashboardWrapper from "../../components/DashboardWrapper";
 import { useLoggedInUser } from "../../hooks/useLoggedInUser";
-import { LoggedInUserEditForm } from "../../types";
+import { GetLoggedInUserResponse, LoggedInUserEditForm } from "../../types";
 
-const member = () => {
+type UserEditorForm = Omit<GetLoggedInUserResponse, "id" | "role">;
+
+const Editable = ({
+  label,
+  controlName,
+  control,
+  isEditing,
+  placeholder,
+}: {
+  label: string;
+  controlName: keyof UserEditorForm;
+  control: Control<UserEditorForm, any>;
+  isEditing: boolean;
+  placeholder?: string;
+}) => (
+  <div>
+    <Label text={label} />
+    <Controller
+      control={control}
+      name={controlName}
+      render={({ field: { onChange, value } }) => (
+        <input
+          className="block w-full py-2 px-3 rounded-md border-solid border-black border-2 disabled:p-0 disabled:border-none"
+          onChange={onChange}
+          value={value}
+          disabled={!isEditing}
+          placeholder={placeholder}
+        />
+      )}
+    />
+  </div>
+);
+
+const Label = ({ text }: { text: string }) => (
+  <label className="block mb-1 font-semibold">{text}</label>
+);
+
+const Divider = () => <div className="w-full h-px bg-black my-4"></div>;
+
+const Member = () => {
   // mock data until auth is confidently correct
   const mock = {
     userId: "clbhseq990000pjmt5jrtkmu1",
@@ -12,18 +51,18 @@ const member = () => {
 
   const user = useLoggedInUser(mock.userId);
 
-  const { formState, handleSubmit, control, setValue, reset } = useForm({
-    defaultValues: {
-      vId: "",
-      username: "",
-      displayName: "",
-      firstName: "",
-      lastName: "",
-      imageUrl: "",
-      github: "",
-      role: "",
-    },
-  });
+  const { formState, handleSubmit, control, setValue, reset } =
+    useForm<UserEditorForm>({
+      defaultValues: {
+        vId: "",
+        username: "",
+        displayName: "",
+        firstName: "",
+        lastName: "",
+        imageUrl: "",
+        github: "",
+      },
+    });
 
   // populate form with values
   useEffect(() => {
@@ -43,69 +82,125 @@ const member = () => {
   return (
     <DashboardWrapper title="Member">
       <>
-        <div>
-          [MOCK CHECKMARK] Changes made to the starred (*) fields will reflect
-          publicly on this site
-        </div>
         {/* Display inputs with these as the placeholder */}
         {/* This might just be the github display photo, or at least defaulted to it */}
         {user.isLoading && <div>Loading</div>}
         {user.data && (
           <>
-            {isEditing ? (
-              <div>
-                <Controller
+            <div>
+              <h2>Public Info</h2>
+              <p className="pb-4">
+                This information is publicly available on this website. (some
+                privacy disclaimers maybe?)
+              </p>
+              <div className="grid grid-cols-2 gap-4 mb-4">
+                <Editable
+                  label="Username"
                   control={control}
-                  name="firstName"
-                  render={({ field: { onChange, value } }) => (
-                    <input
-                      onChange={onChange}
-                      value={value}
-                      placeholder="First Name"
-                    />
-                  )}
+                  isEditing={isEditing}
+                  controlName="username"
+                  placeholder="johnsmith123"
                 />
-
-                {/* TODO: need approval if they edited fields that are publicly visible */}
-                {/* formState.dirtyFields.displayName ? approval button : save changes */}
-                {formState.dirtyFields.displayName && (
-                  <div>
-                    Because you changed your <strong>Display Name</strong>,
-                    admins need to review it for any inappropriate statements
-                    before allowing it
-                  </div>
-                )}
-                <button
-                  className="p-4 bg-green-400"
-                  onClick={handleSubmit(onSubmit)}
-                >
-                  {formState.dirtyFields.displayName
-                    ? "Submit for Approval"
-                    : "Save Changes"}
-                </button>
+                <Editable
+                  label="Display Name"
+                  control={control}
+                  isEditing={isEditing}
+                  controlName="displayName"
+                  placeholder="John Smith"
+                />
               </div>
-            ) : (
-              <div>
-                <div>First Name: {user.data.firstName}</div>
-                <div>Last Name: {user.data.lastName}</div>
-                <div>User Name: user.data.username</div>
-                <div>Display Photo URL: {user.data.imageUrl}</div>
+              <div className="grid grid-cols-2 gap-4 mb-4">
+                <Editable
+                  label="Profile Picture URL"
+                  control={control}
+                  isEditing={isEditing}
+                  controlName="imageUrl"
+                  placeholder="https://cdn.frankerfacez.com/avatar/twitch/2663092323"
+                />
+                <Editable
+                  label="Github Username"
+                  control={control}
+                  isEditing={isEditing}
+                  controlName="github"
+                  placeholder="johnSmithCantCode"
+                />
+              </div>
+
+              <Divider />
+
+              <h2>Private Info</h2>
+              <p className="pb-4">
+                This information will only be seen by the club executives for
+                administration purposes.
+              </p>
+              <div className="grid grid-cols-2 gap-4 mb-4">
+                <Editable
+                  label="First Name"
+                  control={control}
+                  isEditing={isEditing}
+                  controlName="firstName"
+                  placeholder="Johnathan"
+                />
+                <Editable
+                  label="Last Name"
+                  control={control}
+                  isEditing={isEditing}
+                  controlName="lastName"
+                  placeholder="Smith"
+                />
+              </div>
+              <div className="grid grid-cols-2 gap-4 mb-4">
+                <Editable
+                  label="Student ID"
+                  control={control}
+                  isEditing={isEditing}
+                  controlName="vId"
+                  placeholder="01234567"
+                />
                 <div>
-                  GitHub Profile URL: https://github.com/{user.data.github}
+                  <Label text="Club Id" />
+                  <p>{user.data.id}</p>
                 </div>
-                <div>Student ID (only visible to admins): V{user.data.vId}</div>
-                <div>Club ID (cannot be edited): {user.data.id}</div>
-                <div>Role: user.data.role</div>
-                {/* disabled is based on isEditing */}
-
-                <button
-                  className="p-4 bg-orange-400"
-                  onClick={() => setIsEditing(true)}
-                >
-                  Edit Profile
-                </button>
               </div>
-            )}
+              <div>
+                {isEditing ? (
+                  <>
+                    {formState.dirtyFields.displayName && (
+                      <div>
+                        Because you changed your <strong>Display Name</strong>,
+                        admins need to review it for any inappropriate
+                        statements before allowing it
+                      </div>
+                    )}
+                    <button
+                      className="p-4 bg-red-400"
+                      onClick={() => {
+                        setIsEditing(false);
+                        reset(user.data);
+                      }}
+                    >
+                      Cancel Editing
+                    </button>
+                    <button
+                      className="p-4 bg-green-400"
+                      onClick={handleSubmit(onSubmit)}
+                    >
+                      {formState.dirtyFields.displayName
+                        ? "Submit for Approval"
+                        : "Save Changes"}
+                    </button>
+                  </>
+                ) : (
+                  <button
+                    className="p-4 bg-orange-400"
+                    onClick={() => setIsEditing(true)}
+                  >
+                    Edit Profile
+                  </button>
+                )}
+              </div>
+              <Divider />
+            </div>
           </>
         )}
         <h2>DANGER ZONE</h2>
@@ -116,4 +211,4 @@ const member = () => {
   );
 };
 
-export default member;
+export default Member;
