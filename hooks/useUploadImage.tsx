28252995d56d013f227d12620d1dbc5f742b7a29
robchendev <robchendev@gmail.com>
2023-02-08 17:52:46 -0800

    Fxed incorrect mutation type for project image

--- hooks/useUploadImage.tsx
+++ hooks/useUploadImage.tsx
@@ -5,7 +5,9 @@ import { v4 as uuidv4 } from "uuid";
 // Prob dont need this as a hook, will upload all images upon submission.
 export function useUploadProjectImage(projectId: string, userId: string, file: File) {
   const queryClient = useQueryClient();
-  return useMutation<Blob>(["project", projectId], async () => {
+  return useMutation<{
+    path: string;
+  }>(["project", projectId], async () => {
     const { data, error } = await supabase.storage.from("projects").upload(uuidv4(), file);
     if (error) {
       throw new Error(error.message);
